6fd7d0409066a1dbfc329d39fcca598b8cf0e9e3
Andrea Kalife <andreaklfdlg@gmail.com>
2022-03-28 14:31:32 -0600

    Questions

---
+++ assets/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        title: "What role does javascript play in web pages?",
+        choices: ["structures pages", "gives pages their aesthetic layout", "enables interactive functions", "none of the above"],
+        answer: "enables interactive functions"
+    },
+    {
+        title: "Which of these case types isn't commonly used in javascript?",
+        choices: ["camelCase", "snake_case", "PascalCase", "nocase"],
+        answer: "snake_case"
+    },
+    {
+        title: "",
+        choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        choices: [],
+        answer: ""
+    },
+    {
+        title: "",
+        choices: [],
+        answer: ""
+    },
+];
