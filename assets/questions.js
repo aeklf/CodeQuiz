651576b5b4a258afc1b18ece03f14fc5d528a59b
Andrea Kalife <andreaklfdlg@gmail.com>
2022-03-28 14:46:22 -0600

    Assets

--- assets/questions.js
+++ assets/questions.js
@@ -10,43 +10,43 @@ var questions = [
         answer: "snake_case"
     },
     {
-        title: "",
-        choices: [],
-        answer: ""
+        title: "If you declare a variable (e. var name = Andrea) and later assign another value (e. name = Jason) which value will appear in the console log?",
+        choices: ["Andrea", "Jason", "Neither", "Both"],
+        answer: "Jason"
     },
     {
-        title: "",
-        choices: [],
-        answer: ""
+        title: "Booleans, numbers and strings are all types of _.",
+        choices: ["Primitive Values", "Secondary Values", "JS Elements", "Variable IDs"],
+        answer: "Primitive Values"
     },
     {
-        title: "",
-        choices: [],
-        answer: ""
+        title: "Inside which element to you link your javascript to your HTML file?",
+        choices: ["<script>", "<js>", "<javascript>", "<a>"],
+        answer: "<script>"
     },
     {
-        title: "",
-        choices: [],
-        answer: ""
+        title: "How would you write something in an alert box?",
+        choices: ["alertBox()", "msg()", "alert()", "msgBox()"],
+        answer: "alert()"
     },
     {
-        title: "",
-        choices: [],
-        answer: ""
+        title: "Which of the following is the correct format for functions?",
+        choices: ["function = myFunction", "function()", "function:myFunction()", "function myFunction()"],
+        answer: "function myFunction()"
     },
     {
-        title: "",
-        choices: [],
-        answer: ""
+        title: "How do you add comments on javascript?",
+        choices: ["/* Comment */", "<!-- Comment -->", "'Comment'", "//Comment//"],
+        answer: "//Comment//"
     },
     {
-        title: "",
-        choices: [],
-        answer: ""
+        title: "How do you declare a variable in javascript?",
+        choices: ["variable: Name;", "var=Name;", "var:Name;", "var Name;"],
+        answer: "var Name;"
     },
     {
-        title: "",
-        choices: [],
-        answer: ""
+        title: "Which sign is used to assign value to a variable?",
+        choices: ["*", "-", "=", ":"],
+        answer: "="
     },
 ];
